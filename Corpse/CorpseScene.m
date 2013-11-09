587d65d95bd931df2b1b7e4e9b89e659d9a81673
Kevin Scott <kevin@scottlabsllc.com>
2013-11-09 15:12:09 -0500

    Allows you to play multiple times

--- Corpse/CorpseScene.m
+++ Corpse/CorpseScene.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2013 Insanely Awesome. All rights reserved.
 //
 
+#import "MenuScene.h"
 #import "CorpseScene.h"
 
 @interface CorpseScene ()
@@ -27,6 +28,9 @@
     [self setBackgroundColor:[SKColor colorWithWhite:1 alpha:1]];
     [self setScaleMode:SKSceneScaleModeAspectFit];
     [self addChild: [self newWinNode]];
+    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(playAgainNode) userInfo:nil repeats:NO];
+
+
 }
 
 - (SKLabelNode *)newWinNode
@@ -38,5 +42,38 @@
     [winNode setPosition:CGPointMake(CGRectGetMidX([self frame]), CGRectGetMidY([self frame]))];
     return winNode;
 }
+
+- (void)playAgainNode
+{
+    SKLabelNode *winNode = [SKLabelNode labelNodeWithFontNamed:@"Emulogic"];
+    [winNode setFontColor:[SKColor colorWithWhite:0 alpha:1]];
+    [winNode setText:@"Play Again?"];
+    [winNode setFontSize:14];
+    [winNode setName:@"playAgain"];
+    [winNode setPosition:CGPointMake(CGRectGetMidX([self frame]), CGRectGetMidY([self frame])-150)];
+    [winNode setAlpha:0];
+    
+    [self addChild: winNode];
+    
+    SKAction *fadeAway = [SKAction fadeInWithDuration:0.5];
+    SKAction *zoomAndFade = [SKAction group:@[fadeAway]];
+    SKAction *moveSequence = [SKAction sequence:@[zoomAndFade]];
+    [winNode runAction:moveSequence ];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    SKNode *menuNode = [self childNodeWithName:@"playAgain"];
+    if (menuNode != nil)
+    {
+        [menuNode setName:nil];
+        SKScene *menuScene = [[MenuScene alloc] initWithSize:[self size]];
+        SKTransition *doors = [SKTransition doorsOpenHorizontalWithDuration:0.75];
+        [[self view] presentScene:menuScene transition:doors];
+
+    }
+}
+
+
 
 @end
